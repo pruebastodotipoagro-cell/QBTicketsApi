5674f1bf91ad06bc1600f8a93c55d6f2a0ccea5a
Edwins <pruebastodotipoagro@email.com>
2026-06-24 16:26:38 -0600

    a

--- QBTicketsApi/Program.cs
+++ QBTicketsApi/Program.cs
@@ -16,13 +16,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
