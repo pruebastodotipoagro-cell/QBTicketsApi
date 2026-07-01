b8a41533533dc16433d1401b4c6640abf53a39ff
Edwins <pruebastodotipoagro@email.com>
2026-07-01 11:13:24 -0600

    A

---
+++ QBTicketsApi/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QBTicketsApi.Services;
+
+namespace QBTicketsApi.Controllers
+{
+    [ApiController]
+    [Route("api/customers")]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet("nit-test")]
+        public IActionResult NitTest([FromServices] CustomerLookupService lookup)
+        {
+            return Ok(new
+            {
+                customer = "ALEJANDRO REYES, PEDRO",
+                nit = lookup.GetNit("ALEJANDRO REYES, PEDRO")
+            });
+        }
+    }
+}
